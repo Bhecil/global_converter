9da86f9ace3b963a36ba486e63a6d790c2c76108
PERNET Vincent <pernet.vincent@gmail.com>
2025-06-25 20:55:12 +0200

    Basic input for string to convert

--- GlobalConverter.java
+++ GlobalConverter.java
@@ -1,5 +1,11 @@
+
 public class GlobalConverter {
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Invalid number of arguments.");
+            return;    
+        }
+        String stringToConvert = args[0];
     }
 }
