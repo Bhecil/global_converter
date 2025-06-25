e6409e364ff37ecddde05c6c17dc033933d759b1
PERNET Vincent <pernet.vincent@gmail.com>
2025-06-25 20:57:00 +0200

    Added string validation method

--- GlobalConverter.java
+++ GlobalConverter.java
@@ -6,6 +6,18 @@ public class GlobalConverter {
             System.out.println("Invalid number of arguments.");
             return;    
         }
+        
         String stringToConvert = args[0];
+
+        if (IsStringValid(stringToConvert) == false) {
+            System.out.println("Invalid String.");
+        }
+        else {
+            System.out.println("Valid String");
+        }
+    }
+
+    private static boolean IsStringValid(String stringToConvert) {
+        return (stringToConvert != null) && stringToConvert.matches("^[a-zA-Z0-9- ]+$");
     }
 }
