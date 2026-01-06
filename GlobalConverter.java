06bc2af07cade919437cbce4e096ee417eaef34d
PERNET Vincent <pernet.vincent@gmail.com>
2026-01-06 13:57:44 +0100

    refactoring and input/base validity check

--- GlobalConverter.java
+++ GlobalConverter.java
@@ -1,54 +1,48 @@
 import java.util.Scanner;
 
 public class GlobalConverter {
-    static String input;
-    static String base;
+    static String input = "";
+    static String base = "";
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        gatherInput(args);
-
-        if (isValidInput(input)) {
-            System.out.println("Valid input");
-        } else {
-            System.out.println("Invalid input.\nMust be alphanumerical");
-        }
-
-        if (isValidBase(base)) {
-            System.out.println("Valid base");
-        } else {
-            System.out.println("Invalid base.\nAvailable bases:\n -o octal\n -h hexadecimal\n -b binary\n -t text");
-        }
-
-        System.out.println(toASCII(input));
-
-    }
-
-    private static void gatherInput(String[] args) {
         if (args.length >= 2) {
             // default input
             input = args[1];
             base = args[0];
-        } else {
-            // manual input
-            System.out.println("Enter string to convert:");
-            Scanner sc = new Scanner(System.in);
-            input = sc.nextLine();
-
-            System.out.println("Enter base:");
-            base = sc.nextLine();
-
-            sc.close();
         }
 
+        GetValidInput();
+        GetValidBase();
+
+        System.out.println(toASCII(input));
+
+        sc.close();
     }
 
-    private static boolean isValidBase(String base) {
-        return base.matches("-[ohbt]");
+    private static void GetValidInput() {
+        while (!isValidInput()) {
+            System.out.println("Enter an alphanumerical string to convert:");
+            input = sc.nextLine();
+        }
     }
 
-    private static boolean isValidInput(String input) {
-        return input.matches("[A-Za-z0-9 ]+");
+    private static void GetValidBase() {
+        while (!isValidBase()) {
+            System.out.println("Available bases:\n -o octal\n -h hexadecimal\n -b binary\n -t text");
+            System.out.println("Enter a valid base:");
+            base = sc.nextLine();
+        }
+    }
+
+    private static boolean isValidBase() {
+        return base.length() == 2 && base.matches("-[ohbt]");
+    }
+
+    private static boolean isValidInput() {
+        return input.length() > 0 && input.matches("[A-Za-z0-9 ]+");
     }
 
     private static String toASCII(String input) {
